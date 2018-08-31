5fa0346fbe7190cb273f0a0266a1e4882b73f78e
Albert Stjärne <albert.stjarne@gmail.com>
2018-08-31 17:17:09 +0200

    final version

--- car.rb
+++ car.rb
@@ -1,10 +1,15 @@
 class Car
 
-attr_accessor :color
+attr_accessor :color, :driver
 
 def paint(color)
   @color = color
 end
+
+def passenger(driver)
+  @driver = driver
+end
+
 
 end
 
--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -2,9 +2,17 @@ require './car.rb'
 
 describe Car do
 
+  
+let(:driver) {instance_double('Driver')}
+
 it 'has a color' do
   subject.color = "pink"
   expect(subject.color).to eq "pink"
+end
+
+it 'has a driver' do
+    subject.passenger(driver)
+  expect(subject.driver).to be driver
 end
 
 
